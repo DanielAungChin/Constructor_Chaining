e35a97a28a4cac6c4a4ff1984e8ad7826fbea1bb
DanielChin <76833985+DanielAungChin@users.noreply.github.com>
2021-02-22 22:12:40 +0900

    first

---
+++ Constructor_Chaining/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User("This is MY WORLD", "WEARMLY WELCOME.");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        class User
+        {
+            public User()
+            {
+                Console.Write("Hello!!!");
+            }
+
+            public User(string s) : this()
+            {
+                Console.Write(s);
+            }
+            public User(string s1,string s2) : this("Welcome...")
+            {
+                Console.Write(s1 + ">>>" + s2);
+            }
+        }
+    }
+}
